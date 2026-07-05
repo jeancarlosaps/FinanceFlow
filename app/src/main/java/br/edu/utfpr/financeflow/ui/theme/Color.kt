ad975f16b17d564c80fb443f81ec0534b75b4e3c
Jean Carlos <6986880+jeancarlosaps@users.noreply.github.com>
2026-07-05 15:57:11 -0300

    feat: create Material 3 theme

---
+++ app/src/main/java/br/edu/utfpr/financeflow/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package br.edu.utfpr.financeflow.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Green40 = Color(0xFF2E7D46)
+val Green80 = Color(0xFF8FD9A4)
+val GreenGrey40 = Color(0xFF4F6353)
+val GreenGrey80 = Color(0xFFB6CCB9)
+val Sand40 = Color(0xFF7A5900)
+val Sand80 = Color(0xFFEFC03F)
+
+val IncomeGreen = Color(0xFF2E7D46)
+val IncomeGreenDark = Color(0xFF8FD9A4)
+val ExpenseRed = Color(0xFFC62828)
+val ExpenseRedDark = Color(0xFFF2A6A6)
